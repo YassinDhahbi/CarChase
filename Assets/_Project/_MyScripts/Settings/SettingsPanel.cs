7fa0c7d91b0f1aa392628c2d21554acda497be59
YassinDhahbi <dhahbiyy@gmail.com>
2024-09-02 15:11:16 +0200

    Added the settings menu

---
+++ Assets/_Project/_MyScripts/Settings/SettingsPanel.cs
@@ -0,0 +1,55 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsPanel : MonoBehaviour
+{
+    [SerializeField] private Button _seetingsBtn;
+    [SerializeField] private Button _backBtn;
+    [SerializeField] private GameObject _settingsParameters;
+    [SerializeField] private GameObject _pauseTitle;
+    [SerializeField] private GameObject _pauseMenu;
+    [SerializeField] private Slider _sfxSlider;
+    [SerializeField] private Slider _musicSlider;
+
+    private void OnEnable()
+    {
+        _seetingsBtn.onClick.AddListener(OpenSettings);
+        _backBtn.onClick.AddListener(CloseSettings);
+        _sfxSlider.onValueChanged.AddListener(ManageSFXVolume);
+        _musicSlider.onValueChanged.AddListener(ManageMusicVolume);
+    }
+    private void OnDisable()
+    {
+        _seetingsBtn.onClick.RemoveListener(OpenSettings);
+        _backBtn.onClick.RemoveListener(CloseSettings);
+        _sfxSlider.onValueChanged.RemoveListener(ManageSFXVolume);
+        _musicSlider.onValueChanged.RemoveListener(ManageMusicVolume);
+    }
+    private void CloseSettings()
+    {
+        _settingsParameters.SetActive(false);
+        _seetingsBtn.gameObject.SetActive(true);
+        _pauseTitle.SetActive(true);
+    }
+
+    private void OpenSettings()
+    {
+        _settingsParameters.SetActive(true);
+        _seetingsBtn.gameObject.SetActive(false);
+        _pauseTitle.SetActive(false);
+
+    }
+
+    private void ManageSFXVolume(float value) => AudioManager.Instance.SetSFXVolume(value);
+    private void ManageMusicVolume(float value) => AudioManager.Instance.SetMusicVolume(value);
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            var currentState = _pauseMenu.activeInHierarchy;
+            _pauseMenu.SetActive(!currentState);
+        }
+    }
+}

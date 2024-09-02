c40b5b196e4af59d38c6786e7938b0bf96f0af81
YassinDhahbi <dhahbiyy@gmail.com>
2024-09-02 12:47:34 +0200

    Added the city prefab

--- Assets/_Project/_MyScripts/FuelManager/FuelManager.cs
+++ Assets/_Project/_MyScripts/FuelManager/FuelManager.cs
@@ -1,20 +1,27 @@
+using ArcadeVehicleController;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class FuelManager : MonoBehaviour
 {
-    [SerializeField] private float _maxFuel = 100f;
+    [SerializeField] private VehicleSettings _vehicleSettingsSO;
     [SerializeField] private float _fuel = 100f;
     [SerializeField] private Image _fuelImage;
+    [SerializeField] private Vehicle _vehicle;
     void Start()
     {
-        _fuel = _maxFuel;
+        _fuel = _vehicleSettingsSO.Fuel;
     }
 
     void Update()
     {
+        if (_vehicle == null) return;
+        if (_vehicle.Velocity.magnitude > 1f)
+        {
+            _fuel -= Time.deltaTime;
+            _fuelImage.fillAmount = _fuel / _vehicleSettingsSO.Fuel;
+        }
 
-        _fuelImage.fillAmount = _fuel / _maxFuel;
     }
 
 }
